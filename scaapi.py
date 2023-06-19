9ad89debe8514360f165d3b2c1f6eb230fbb2a6a
eroux <elie.roux@telecom-bretagne.eu>
2023-06-19 12:28:14 +0200

    first working version, split preprocessing script

--- scaapi.py
+++ scaapi.py
@@ -1,36 +1,15 @@
 from datetime import datetime
+from flask import Flask, json, request
 import pickle
 import gzip
-from ./cal_sget_sam_output
+from utils import upload_to_s3, gets3blob
 
-VERSION = 0.0.1
+from flask_cors import CORS
 
-DEFAULT_PREPROCESS_OPTIONS = {
-    "pps": 8,
-    "sam_resize": 1024,
-    "thumbnail_resize": 512,
-    "pre_rotate": 0,
-    "use_exif_rotation": False,
-    "grayscale_thumbnail": False
-}
+api = Flask("SCAM-QC", static_url_path='', static_folder='web/')
+CORS(api)
 
-def get_pickle_path(img_path):
-    return "sam_pickle_gz/"+img_path+"_sam_pickle.gz"
-
-def save_thumbnail(folder_path, img_path, pil_img, preprocess_options):
-    ratio = max(preprocess_options["thumbnail_resize"] / pil_img.width, preprocess_options["thumbnail_resize"] / pil_img.height)
-    new_width = int(pil_img.width * ratio)
-    new_height = int(pil_img.height * ratio)
-    pil_img = pil_img.resize((new_width, new_height), Image.LANCZOS)
-    if preprocess_options["pre_rotate"] != 0:
-        pil_img = pil_img.rotate(preprocess_options["pre_rotate"], expand=True)
-    byts, ext = encode_img(pil_img, mozjpeg_optimize=True)
-    path = "thumbnails/"+img_path+ext
-    upload_to_s3(byts, path)
-
-def save_sam_pickle(pickle_path, sam_res):
-    pickle_bytes = get_gzip_picked_bytes(sam_res)
-    upload_to_s3(pickle_bytes, pickle_path)
+VERSION = "0.0.1"
 
 def get_gz_pickle(pickle_path):
     blob = gets3blob(pickle_path)
@@ -39,69 +18,14 @@ def get_gz_pickle(pickle_path):
     blob.seek(0)
     return pickle.loads(gzip.decompress(blob.read()))
 
-def get_pil_img(folder_path, img_path):
-    img = Image.open(gets3blob(folder_path+img_path))
-    img = apply_exif_rotation(img)
-    return img
-
-def run_sam(pil_img, preprocess_options):
-
-
-def preprocess_folder(folder_path, preprocess_options=DEFAULT_PREPROCESS_OPTIONS):
-    """
-    pre-processes a folder for use with the API
-
-    - run SAM and save pickles
-    - generates and writes thumbnails
-    - writes scam.json in the directory
-    """
-    img_paths = sorted(get_all_img_paths(folder_path))
-    scam_json = {
-        "preprocess_run": {
-            "date": datetime.now().isoformat(),
-            "version": VERSION,
-            "preprocess_options": preprocess_options,
-        }
-        "checked": False,
-        "folder_path": folder_path,
-        "scam_runs": []
-        "files": {}
-    }
-    files = scam_json["files"]
-    for img_path in img_paths:
-        # pil_img is not rotated
-        pil_img = get_pil_img(folder_path, img_path, preprocess_options)
-        sam_res = run_sam(pil_img, preprocess_options)
-        pickle_path = get_pickle_path(folder_path, img_path)
-        save_sam_pickle(pickle_path, sam_res)
-        # thumbnail will get rotated
-        thumbnail_path, w, h = save_thumbnail(folder_path, img_path, pil_img, preprocess_options)
-        files.append({
-            "img_path": img_path,
-            "pickle_path": pickle_path,
-            "width": pil_img.width,
-            "height": pil_img.height,
-            "rotation": preprocess_options["pre_rotate"], # can be modified by users
-            "thumbnail_path": thumbnail_path,
-            "thumbnail_info": {
-                "width": w,
-                "height": h,
-                "rotation": preprocess_options["pre_rotate"] # inherent to the image, cannot be modified
-            }
-        })
-    save_scam_json(folder_path, scam_json)
-
 def get_thumbnail_bytes(folder_path, thumbnail_path):
     """
     returns the bytes of the thumbnail
     """
     return gets3blob(thumbnail_path)
 
-DEFAULT_RUN_OPTIONS = {
-    "alter_checked": False,
-}
-
 DEFAULT_SCAM_OPTIONS = {
+    "alter_checked": False,
     "direction": "vertical",
     "squarishness_min": 0.85,
     "squarishness_min_warn": 0.7,
@@ -118,7 +42,7 @@ DEFAULT_SCAM_OPTIONS = {
     "cut_at_fixed": False
 }
 
-def run_scam_folder(folder_path, run_options = DEFAULT_RUN_OPTIONS, scam_options = DEFAULT_SCAM_OPTIONS):
+def run_scam_folder(folder_path, scam_options = DEFAULT_SCAM_OPTIONS):
     """
     runs scam on a folder. The folder must have been preprocessed and have a scam.json file
     """
@@ -129,23 +53,22 @@ def run_scam_folder(folder_path, run_options = DEFAULT_RUN_OPTIONS, scam_options
     scam_json["scam_runs"].append({
         "date": datetime.now().isoformat(),
         "version": VERSION,
-        "run_options": run_options,
         "scam_options": scam_options
     })
     for file_info in scam_json["files"]:
-        if file_info["checked"] and not run_options["alter_checked"]:
+        if file_info["checked"] and not scam_options["alter_checked"]:
             continue
         sam_anns = get_gz_pickle(file_info["pickle_path"])
         add_scam_results(file_info, sam_anns, scam_options)
-    save_scam_json(folder_path, scam_json)
+    return scam_json
 
-def run_scam_image(folder_path, file_info):
+def run_scam_image(folder_path, file_info, scam_options):
     sam_anns = get_gz_pickle(file_info["pickle_path"])
     add_scam_results(file_info, sam_anns, scam_options)
     return file_info
 
 def save_scam_json(folder_path, scam_json_obj):
-    scam_json_str = json.dumps(scam_json, indent=2)
+    scam_json_str = json.dumps(scam_json_obj, indent=2)
     json_file_path = folder_path+"scam.json"
     upload_to_s3(scam_json_str.encode('utf-8'), json_file_path)
 
@@ -156,3 +79,33 @@ def get_scam_json(folder_path):
         return None
     blob.seek(0)
     return json.loads(blob.read().decode("utf-8"))
+
+@api.route('/save_scam_json', methods=['POST'])
+def save_scam_json_api():
+    data = request.json
+    folder_path = data.get('folder_path')
+    scam_json_obj = data.get('scam_json_obj')
+
+@api.route('/get_scam_json', methods=['POST'])
+def get_scam_json_api():
+    data = request.json
+    folder_path = data.get('folder_path')
+    return get_scam_json(folder_path)
+
+@api.route('/run_scam_file', methods=['POST'])
+def run_scam_file_api():
+    data = request.json
+    folder_path = data.get('folder_path')
+    folder_path = data.get('file_info')
+    folder_path = data.get('scam_options')
+    return run_scam_image(folder_path, file_info, scam_options)
+
+@api.route('/run_scam_folder', methods=['POST'])
+def run_scam_folder_api():
+    data = request.json
+    folder_path = data.get('folder_path')
+    folder_path = data.get('scam_options')
+    return run_scam_folder(folder_path, scam_options)
+
+if __name__ == '__main__':
+    api.run()
---
+++ scam_preprocess.py
@@ -0,0 +1,114 @@
+import csv
+import sys
+from scaapi import VERSION, save_scam_json
+from utils import list_img_keys, gets3blob, upload_to_s3, get_gzip_picked_bytes
+from datetime import datetime
+import logging
+from PIL import Image
+from cal_sam_pickles import get_sam_output
+from img_utils import apply_exif_rotation, encode_img
+
+DEFAULT_PREPROCESS_OPTIONS = {
+    "pps": 8,
+    "sam_resize": 1024,
+    "thumbnail_resize": 512,
+    "pre_rotate": 0,
+    "use_exif_rotation": False,
+    "grayscale_thumbnail": False
+}
+
+def run_sam(pil_img, preprocess_options):
+    return get_sam_output(pil_img, max_size=preprocess_options["sam_resize"], points_per_side=preprocess_options["pps"])
+
+def save_sam_pickle(pickle_path, sam_res):
+    pickle_bytes = get_gzip_picked_bytes(sam_res)
+    upload_to_s3(pickle_bytes, pickle_path)
+
+def get_pickle_path(folder_path, img_path):
+    return "sam_pickle_gz/"+folder_path+img_path+"_sam_pickle.gz"
+
+def get_all_img_paths(folder_path):
+    img_keys = []
+    for img_full_key in sorted(list_img_keys(folder_path)):
+        img_keys.append(img_full_key[len(folder_path):])
+    return img_keys
+
+def get_pil_img(folder_path, img_path):
+    blob = gets3blob(folder_path+img_path)
+    if blob is None:
+        logging.error("cannot find %s" % (folder_path+img_path))
+    img = Image.open(blob)
+    return img
+
+def save_thumbnail(folder_path, img_path, pil_img, preprocess_options):
+    ratio = max(preprocess_options["thumbnail_resize"] / pil_img.width, preprocess_options["thumbnail_resize"] / pil_img.height)
+    new_width = int(pil_img.width * ratio)
+    new_height = int(pil_img.height * ratio)
+    pil_img = pil_img.resize((new_width, new_height), Image.LANCZOS)
+    if preprocess_options["pre_rotate"] != 0:
+        pil_img = pil_img.rotate(preprocess_options["pre_rotate"], expand=True)
+    byts, ext = encode_img(pil_img, mozjpeg_optimize=True)
+    path = "thumbnails/"+folder_path+img_path+ext
+    upload_to_s3(byts, path)
+    return path, new_width, new_height
+
+def preprocess_folder(folder_path, preprocess_options=DEFAULT_PREPROCESS_OPTIONS):
+    """
+    pre-processes a folder for use with the API
+
+    - run SAM and save pickles
+    - generates and writes thumbnails
+    - writes scam.json in the directory
+    """
+    logging.info("preprocess %s" % folder_path)
+    img_paths = get_all_img_paths(folder_path)
+    logging.info("found %d images" % len(img_paths))
+    scam_json = {
+        "preprocess_run": {
+            "date": datetime.now().isoformat(),
+            "version": VERSION,
+            "preprocess_options": preprocess_options,
+        },
+        "checked": False,
+        "folder_path": folder_path,
+        "scam_runs": [],
+        "files": []
+    }
+    files = scam_json["files"]
+    for img_path in img_paths:
+        # pil_img is not rotated
+        pil_img = get_pil_img(folder_path, img_path)
+        if preprocess_options["use_exif_rotation"]:
+            pil_img = apply_exif_rotation(img)
+        sam_res = run_sam(pil_img, preprocess_options)
+        pickle_path = get_pickle_path(folder_path, img_path)
+        save_sam_pickle(pickle_path, sam_res)
+        # thumbnail will get rotated
+        thumbnail_path, w, h = save_thumbnail(folder_path, img_path, pil_img, preprocess_options)
+        files.append({
+            "img_path": img_path,
+            "pickle_path": pickle_path,
+            "width": pil_img.width,
+            "height": pil_img.height,
+            "rotation": preprocess_options["pre_rotate"], # can be modified by users
+            "thumbnail_path": thumbnail_path,
+            "thumbnail_info": {
+                "width": w,
+                "height": h,
+                "rotation": preprocess_options["pre_rotate"] # inherent to the image, cannot be modified
+            }
+        })
+    save_scam_json(folder_path, scam_json)
+
+
+def preprocess_csv():
+    if len(sys.argv) <= 1:
+        print("nothing to do, please pass the path to a csv file")
+
+    with open(sys.argv[1], newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            preprocess_folder(row[0])
+
+if __name__ == '__main__':
+    preprocess_csv()
