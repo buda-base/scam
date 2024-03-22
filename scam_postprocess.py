da0726e60beeb7af3682123e3a33f61f8968a825
eroux <elie.roux@telecom-bretagne.eu>
2024-03-22 09:17:59 +0100

    try/catch

--- scam_postprocess.py
+++ scam_postprocess.py
@@ -276,8 +276,11 @@ def derive_from_page(scam_json, output_file_info, file_info, pil_img, page_info,
         logging.info("  write to s3 key %s", s3key)
         if not postprocess_options["dryrun"]:
             b, ext = encode_img_uncompressed(extract)
-            sha256 = get_sha256(b)
-            output_file_info["sha256"] = sha256
+            try:
+                sha256 = get_sha256(b)
+                output_file_info["sha256"] = sha256
+            except:
+                logging.error("   cannot compute sha256 for %s", b)
             upload_to_s3(b, s3key)
     else:
         local_path = postprocess_options["local_dst_folder"]
