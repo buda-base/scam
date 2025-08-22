68b32c11257e471b35621a728989c9f63b4076fb
Elie Roux <roux.elie@gmail.com>
2025-08-22 17:40:22 +0200

    oops

--- utils.py
+++ utils.py
@@ -59,7 +59,7 @@ def is_img(path: str) -> bool:
     end4 = os.path.splitext(path)
     if len(end4) < 2:
         return False
-    return end4[1].lower() in [".jpg", ".jpeg", ".tif", ".tiff", ".cr2", ".nef", ".arw", "jp2"]
+    return end4[1].lower() in [".jpg", ".jpeg", ".tif", ".tiff", ".cr2", ".nef", ".arw", ".jp2"]
 
 def list_obj_keys(prefix, bucket=BUCKET_NAME):
     obj_keys = []
