424977f16cf0693c27dbdd8188ff4e458768248d
Elie Roux <roux.elie@gmail.com>
2025-11-21 10:36:12 +0100

    oops

--- dl_results.py
+++ dl_results.py
@@ -301,12 +301,12 @@ def encode_folder(archive_folder, images_folder, ilname, orig_shrink_factor=1.0,
         img_pil = Image.open(io.BytesIO(img_bytes))
 
         file_size = file_stats.st_size
-        if (lastfour == ".jpg" or lastfour == "jpeg")
+        if (lastfour == ".jpg" or lastfour == "jpeg"):
             img_bytes = mozjpeg_lossless_optimization.optimize(img_bytes, copy=mozjpeg_lossless_optimization.COPY_MARKERS.ICC)
             file_size = len(img_bytes)
 
         try:
-            if (lastfour == ".jpg" or lastfour == "jpeg") and file_size < 800 * 1024:
+            if (lastfour == ".jpg" or lastfour == "jpeg") and file_size < 1200 * 1024:
                 ext = ".jpg"
                 # img_bytes already set
                 logging.info("not reencoding %s" % file)
